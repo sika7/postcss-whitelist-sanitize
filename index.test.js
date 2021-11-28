382d1ffcf98b4dcaa3c1b0aa3ea062530143dbbc
sika7 <91676209+sika7@users.noreply.github.com>
2021-11-28 19:45:02 +0900

    add plugin test logic.

--- index.test.js
+++ index.test.js
@@ -1,12 +1,67 @@
-const postcss = require('postcss')
+const postcss = require("postcss");
 
-const plugin = require('./')
+const plugin = require("./");
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined,
+  });
+  expect(result.css).toEqual(output);
+  expect(result.warnings()).toHaveLength(0);
 }
+
+const testOpts = {
+  allowPropertys: ["background-color", "color"],
+  validation: true,
+  allowProperty: true,
+};
+
+it("it works line css !!", async () => {
+  await run(
+    "background-color: #eee;color: #eee;",
+    "background-color: #eee;color: #eee;",
+    testOpts
+  );
+});
+
+it("it works block css !!", async () => {
+  await run(
+    `
+.test {
+  background-color: #eee;
+  color: #eee;
+}
+
+.test2 {
+  a: #eee;
+  color: #eee;
+}
+  `,
+    `
+.test {
+  background-color: #eee;
+  color: #eee;
+}
+
+.test2 {
+  color: #eee;
+}
+  `,
+    testOpts
+  );
+});
+
+it("remove not allowed property", async () => {
+  await run("position: fixed;color: #eee;", "color: #eee;", testOpts);
+});
+
+it("remove unknown property", async () => {
+  await run("background-colr: red;color: #eee;", "color: #eee;", testOpts);
+});
+
+it("remove unknown value", async () => {
+  await run("background-color: hoge;color: #eee;", "color: #eee;", testOpts);
+});
 
 /* Write tests here
 
